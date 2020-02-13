5802b7e55c515cf9520c3e707c4bdc94b4e2a5b3
lpdft <772346730@qq.com>
2020-02-13 10:35:32 +0800

    [ruyue] feat: create Teacher class and add attribute and function

---
+++ modifier/src/main/java/Teacher.java
@@ -0,0 +1,21 @@
+public class Teacher {
+    private final String name;
+    private Student[] student;
+
+    public Teacher(String name, Student[] student) {
+        this.name = name;
+        this.student = student;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Student[] getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student[] student) {
+        this.student = student;
+    }
+}
